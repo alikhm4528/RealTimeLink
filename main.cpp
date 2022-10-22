7dfe84fb1d32a41908a6dd83a4b288fa3850ecb4
ali khadangi <alikhm4528@hotmail.com>
2022-10-22 14:52:54 +0330

    buffer init changed

--- main.cpp
+++ main.cpp
@@ -30,29 +30,29 @@ int main() {
     ReadFromFile* ReadObject;
     WriteToFile* WriteObject;
     ProcessData* ProcessObject;
-    std::queue<uint8_t> inputBuffer;
-    std::queue<uint8_t> outputBuffer;
+    std::queue<uint8_t>* inputBuffer = new std::queue<uint8_t>();
+    std::queue<uint8_t>* outputBuffer = new std::queue<uint8_t>();
 
     std::thread* ReadThread;
     std::thread* WriteThread;
     std::thread* ProcessDataThread;
 
-    ReadObject = new ReadFromFile(&inputBuffer
+    ReadObject = new ReadFromFile(inputBuffer
         , getAddress("inputData.bin")
         , INPUT_BUFFER_SIZE);
-    WriteObject = new WriteToFile(&outputBuffer
+    WriteObject = new WriteToFile(outputBuffer
         , getAddress("outputData.bin")
         , OUTPUT_BUFFER_SIZE);
 
     #ifdef INTERLEAVING
     std::cout << "Interleaving Algorithem\n";
-    ProcessObject = new Interleaving(&inputBuffer
-        , &outputBuffer
+    ProcessObject = new Interleaving(inputBuffer
+        , outputBuffer
         , INPUT_BUFFER_SIZE);
     #else
     std::cout << "Hamming Algorithem\n";
-    ProcessObject = new Hamming(&inputBuffer
-        , &outputBuffer
+    ProcessObject = new Hamming(inputBuffer
+        , outputBuffer
         , INPUT_BUFFER_SIZE);
     #endif
 
